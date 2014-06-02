6a341ad943ca149997c5209ccfdd1ea2d88f6dd1
Victor Velazquez <victor.velazquez@crowdint.com>
2014-06-02 17:53:40 -0500

    Add capybara missing specs

--- spec/features/admin_register_atteendee_spec.rb
+++ spec/features/admin_register_atteendee_spec.rb
@@ -10,6 +10,32 @@ describe "Admin happy path" do
     fill_in "attendee_enterprise", with: "bootcamp"
     fill_in "attendee_curp", with: "oiuyoiu123412"
     fill_in "attendee_carrier", with: "Software Engineer"
+
+
+    within '.date' do
+      within '#attendee_birth_date_3i' do
+        find("option[value='4']").click
+      end
+      within '#attendee_birth_date_2i' do
+        find("option[value='6']").click
+      end
+      within '#attendee_birth_date_1i' do
+        find("option[value='1986']").click
+      end
+    end
+
+    within '#attendee_gender' do
+      find("option[value='Masculino']").click
+    end
+
+    within '#attendee_system_id' do
+      find("option[value='2']").click
+    end
+
+    within '#attendee_size_id' do
+      find("option[value='1']").click
+    end
+
     click_button "Save"
   end
 end
